7c76280c704b51ac7c38c7971918bdebfb3e113d
mytelos <mgksoy@gmail.com>
2019-03-16 17:05:06 +0300

    Basic Command Pattern For Unity As a Baseline

---
+++ Command.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace BasicCommandPattern
+{
+    public abstract class Command
+    {
+        public GameObject Unit;
+        public Command(GameObject Unit, UnityAction ExecFinishedCallback = null)
+        {
+            this.Unit = Unit;
+            if (ExecFinishedCallback != null)
+            {
+                MyExecutionFinished.AddListener(ExecFinishedCallback);
+            }
+        }
+        //public delegate void ExecutionFinished();
+        public UnityEvent MyExecutionFinished;
+        private void OnExecutionFinished()
+        {
+            if (MyExecutionFinished != null)
+            {
+                MyExecutionFinished.Invoke();
+            }
+        }
+
+        public abstract void ExecutionWork();
+
+        public void Execute()
+        {
+            ExecutionWork();
+            OnExecutionFinished();
+        }
+    }
+}
